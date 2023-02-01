0b518f06dd4ee67b01ce935ddb1fd5c75764b032
Vadim <kosvad9@gmail.com>
2023-02-01 11:04:25 +0300

    Урок 3. Условные операторы

--- branching/Task1.java
+++ branching/Task1.java
@@ -4,10 +4,10 @@ public class Task1 {
     public static void main(String[] args) {
         int examResult = 74;
         if (examResult >= 1 && examResult <= 100){
-            if (examResult >= 1 && examResult <= 25) System.out.println("Плохо");
-            else if (examResult >= 26 && examResult <= 50) System.out.println("Ниже среднего");
-            else if (examResult >= 51 && examResult <= 75) System.out.println("Средне");
-            else if (examResult >= 76 && examResult <= 100) System.out.println("Отлично");
+            if (examResult <= 25) System.out.println("Плохо");
+            else if (examResult <= 50) System.out.println("Ниже среднего");
+            else if (examResult <= 75) System.out.println("Средне");
+            else if (examResult <= 100) System.out.println("Отлично");
         }
         else System.out.println("Ошибка");
     }
--- branching/Task2.java
+++ branching/Task2.java
@@ -2,6 +2,32 @@ package branching;
 
 public class Task2 {
     public static void main(String[] args) {
+        final String BANK = "Bank", HEALTH = "Health", TRANSPORT = "Transport";
+        int profit = 350945;
+        int tax;
 
+        String industry = HEALTH;
+        switch (industry){
+            case BANK: {
+                if (profit < 250000) tax = 10;
+                else if (profit < 500000) tax = 15;
+                else tax = 18;
+                break;
+            }
+            case HEALTH: {
+                if (profit < 400000) tax = 7;
+                else if (profit < 900000) tax = 12;
+                else tax = 15;
+                break;
+            }
+            case TRANSPORT: {
+                if (profit < 100000) tax = 9;
+                else if (profit < 300000) tax = 13;
+                else tax = 17;
+                break;
+            }
+            default: tax = 0;
+        }
+        System.out.println("Налог равен "+tax+"%");
     }
 }
