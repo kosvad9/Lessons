52c26db58c9b344381ea40a2eec8314745bf78b8
Vadim <kosvad9@gmail.com>
2023-02-03 15:56:50 +0300

    Урок 4. Работа со строками

---
+++ strings/Task1.java
@@ -0,0 +1,17 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        for(int i = 0; i < numbers.length; i++)
+            numbers[i] = scanner.nextInt();
+        for (int elem: numbers)
+            if (elem % 2 == 0)
+                System.out.printf("Число %d - четное\n",elem);
+            else
+                System.out.printf("Число %d - нечетное\n",elem);
+    }
+}
