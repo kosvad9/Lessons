000e51f98a6c487ef61d938170135a5b3ac2b5fa
Vadim <kosvad9@gmail.com>
2023-02-15 11:08:48 +0300

    Урок 9. Исключения, enum

--- shop/Application.java
+++ shop/Application.java
@@ -1,11 +1,26 @@
 package shop;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
     public static Scanner sc = new Scanner(System.in);
+
+    private Product[] products = new Product[]{new Product("Смартфон","Apple Iphone 14 128GB",899.99,Categories.ELECTRONICS),
+            new Product("Телевизор","Samsung",754.99,Categories.ELECTRONICS),
+            new Product("Чехол","Чехол для смартфона Apple Iphone 14",3.00,Categories.ACCESSORIES),
+            new Product("Кровать","Двухместная кровать",120.40,Categories.FURNITURE)};
     public static void main(String[] args) {
+        Application app = new Application();
         Menu menu = new Menu();
-        menu.printMenu();
+        while (true){
+            //try{
+                switch (menu.printMenu()){
+                    case SHOWPRODUCTS -> {
+                        break;
+                    }
+                }
+            //}
+        }
     }
 }
--- shop/Menu.java
+++ shop/Menu.java
@@ -1,11 +1,28 @@
 package shop;
 
+import java.util.InputMismatchException;
+
 public class Menu {
-    public void printMenu(){
+    public MenuItems printMenu(){
         MenuItems[] mi = MenuItems.values();
         System.out.println("Выберите действие:");
         for (int i = 0; i < mi.length; i++){
             System.out.printf("%d. %s;\n",i,mi[i].getDescriprion());
+        }
+        return choiceMenu();
+    }
+
+    private MenuItems choiceMenu(){
+        while(true){
+            try{
+                MenuItems choiceItem = MenuItems.values()[Application.sc.nextInt()];
+                return choiceItem;
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Выбран несуществующий пункт меню! Повторите попытку.");
+            }catch (InputMismatchException e){
+                System.out.println("Введены некорректные данные! Повторите попытку.");
+                Application.sc.nextLine();
+            }
         }
     }
 }
