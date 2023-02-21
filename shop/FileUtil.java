d569c4061fbf9121392322f60a912858d15c1408
Vadim <kosvad9@gmail.com>
2023-02-21 13:03:07 +0300

    Урок 10. Потоки ввода и вывода

--- shop/FileUtil.java
+++ shop/FileUtil.java
@@ -9,21 +9,28 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileUtil {
-    public static void readProducts(){
+    private static Product[] defaultProducts = new Product[]{new Product("Смартфон","Apple Iphone 14 128GB",899.99,Category.ELECTRONICS),
+            new Product("Телевизор","Samsung",754.99,Category.ELECTRONICS),
+            new Product("Чехол","Чехол для смартфона Apple Iphone 14",3.00,Category.ACCESSORIES),
+            new Product("Кровать","Двухместная кровать",120.40,Category.FURNITURE),
+            null, null, null, null, null, null};
+    public static Product[] readProducts(){
         Path path = Paths.get("./shop","products.csv");
+        if (!Files.exists(path)) return defaultProducts;
         try {
             Stream<String> lines = Files.lines(path);
             String[] stringsProduct = (String[])lines.toArray();
             Product[] products = new Product[stringsProduct.length+1];
             for (int i = 0; i < stringsProduct.length; i++)
-                products[i] = getProductFromCSV(stringsProduct[i]);
+                products[i] = parseProductFromCSV(stringsProduct[i]);
+            return products;
         }catch (IOException e){
             System.out.println("При извлечении данных произошла ошибка "+e);
         }
-
+        return defaultProducts;
     }
 
-    private static Product getProductFromCSV(String str){
+    private static Product parseProductFromCSV(String str){
         try {
             String[] params = str.split(",");
             String name = params[0];
