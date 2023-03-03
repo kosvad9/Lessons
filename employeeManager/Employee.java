2ecd757c1d74f619c477b7716dc16bf2c0ff18fc
Vadim <kosvad9@gmail.com>
2023-03-03 14:17:09 +0300

    Урок 2. List

---
+++ employeeManager/Employee.java
@@ -0,0 +1,31 @@
+package employeeManager;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Employee{
+    private String firstName;
+    private String lastName;
+    private int id;
+    private Position position;
+    private LocalDate dateOfEmployment;
+    private BigDecimal salary;
+    private LocalDate salaryRevisionDate;
+
+    public Employee(String firstName, String lastName, int id, Position position, LocalDate dateOfEmployment, BigDecimal salary, LocalDate salaryRevisionDate){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.position = position;
+        this.dateOfEmployment = dateOfEmployment;
+        if (salary.compareTo(position.getMinSalary()) >= 0 && salary.compareTo(position.getMaxSalary()) <= 0)
+            this.salary = salary;
+        else
+            throw new IllegalArgumentException(String.format("Для работника с id \'%s\' указанная ЗП вне диапазона выбранной должности!",id));
+        this.salaryRevisionDate = salaryRevisionDate;
+    }
+
+    public Employee(String firstName, String lastName, int id, Position position, BigDecimal salary) {
+        this(firstName,lastName,id,position,LocalDate.now(),salary,null);
+    }
+}
--- employeeManager/EmployeeManager.java
+++ employeeManager/EmployeeManager.java
@@ -1,7 +1,26 @@
 package employeeManager;
 
-public class EmployeeManager {
-    public static void main(String[] args) {
+import employeeManager.positions.development.MiddleDeveloper;
+import employeeManager.positions.development.TeamLead;
+import employeeManager.positions.hr.Manager;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EmployeeManager {
+
+    List<Employee> staff;
+    public static void main(String[] args) {
+        try {
+            EmployeeManager em = new EmployeeManager();
+            em.staff = new LinkedList<>();
+            em.staff.add(new Employee("Вадим", "Космачёв", 1, new TeamLead(), new BigDecimal(7000)));
+            em.staff.add(new Employee("Иван", "Сидоренко", 2, new MiddleDeveloper(), new BigDecimal(1500)));
+            em.staff.add(new Employee("Екатерина", "Петрова", 3, new Manager(), new BigDecimal(801)));
+
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
