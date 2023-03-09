46bf9581185084f45454815e5d228b11c34fb25c
Vadim <kosvad9@gmail.com>
2023-03-09 15:39:19 +0300

    Урок 3. HashMap

--- employeeManager/Accounting.java
+++ employeeManager/Accounting.java
@@ -4,9 +4,9 @@ import java.math.BigDecimal;
 
 public class Accounting {
     public static void changeSalary(Employee employee, BigDecimal newSalary){
-        if (newSalary.compareTo(employee.getPosition().getMinSalary()) >= 0 &&
-                newSalary.compareTo(employee.getPosition().getMaxSalary()) <= 0)
+//        if (newSalary.compareTo(employee.getPosition().getMinSalary()) >= 0 &&
+//                newSalary.compareTo(employee.getPosition().getMaxSalary()) <= 0)
         employee.setSalary(newSalary);
-        else System.out.printf("Для работника \'%s %s\' указанная ЗП вне диапазона выбранной должности!",employee.getFirstName(),employee.getLastName());
+//        else System.out.printf("Для работника \'%s %s\' указанная ЗП вне диапазона выбранной должности!",employee.getFirstName(),employee.getLastName());
     }
 }
